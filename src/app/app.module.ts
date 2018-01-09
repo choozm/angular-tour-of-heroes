f97355c6846a0a33ce6411dbc38f2a415d8a115e
Choo Zhi Min <choozhimin@gmail.com>
2018-01-09 17:46:58 +0800

    start fresh and upgrade to angular 5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,18 @@
-import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }   from '@angular/forms'; // <-- NgModel lives here
-import { AppComponent }  from './app.component';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+
 
 @NgModule({
-  imports:      [ BrowserModule,  
-                  FormsModule // <-- import the FormsModule before binding with [(ngModel)]
-                ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
